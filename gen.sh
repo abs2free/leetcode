8192dbc6aded7afa57ba8529b75f4a1cd3a0fd0e
abs2free <abs2free@gmail.com>
2024-03-29 10:54:02 +0800

    write generate file

---
+++ gen.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+echo "// package main this is a leetcode
+package main
+
+/*
+*
+
+*
+ */
+func $2(in string) string {
+    return \"\"
+}" > $1.go;
+
+
+foo=$2;
+upperName="$(tr '[:lower:]' '[:upper:]' <<< ${foo:0:1})${foo:1}"
+
+echo "package main
+
+import \"testing\"
+
+var $2Cases = []struct {
+	name   string
+	input  string
+	except string
+}{
+	{
+	},
+}
+
+func Test$upperName(t *testing.T) {
+	t.Parallel()
+	for _, c := range $2Cases {
+		t.Run(c.name, func(t *testing.T) {
+			actual := $2(c.input)
+			if actual != c.except {
+				t.Errorf(\"$2 test has fail: input:%v ,except:%v, actual:%v \\\n\", c.input, c.except, actual)
+			}
+		})
+	}
+}" > "$1_test.go"
