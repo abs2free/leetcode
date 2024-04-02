d699b0467eb1de2b56936e3048ac11902ec5faaa
abs2free <abs2free@gmail.com>
2024-04-02 14:48:47 +0800

    66. 加一

---
+++ 66.plus-one_test.go
@@ -0,0 +1,46 @@
+package main
+
+import "testing"
+
+var plusOneCases = []struct {
+	name   string
+	input  []int
+	except []int
+}{
+
+	{
+		"test1",
+		[]int{1, 2, 3},
+		[]int{1, 2, 4},
+	},
+
+	{
+		"test4",
+		[]int{4, 3, 2, 1},
+		[]int{4, 3, 2, 2},
+	},
+
+	{
+		"test2",
+		[]int{9, 9},
+		[]int{1, 0, 0},
+	},
+
+	{
+		"test3",
+		[]int{0},
+		[]int{1},
+	},
+}
+
+func TestPlusOne(t *testing.T) {
+	t.Parallel()
+	for _, c := range plusOneCases {
+		t.Run(c.name, func(t *testing.T) {
+			actual := plusOne(c.input)
+			if IntsNotEqual(actual, c.except) {
+				t.Errorf("plusOne %s test  has fail: input:%v ,except:%v, actual:%v \n", c.name, c.input, c.except, actual)
+			}
+		})
+	}
+}
