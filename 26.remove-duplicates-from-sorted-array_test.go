d8a1baa6af75dd83de56471cf40b183a5abe5805
abs2free <abs2free@gmail.com>
2024-04-02 11:32:45 +0800

    26. 删除有序数组中的重复项

---
+++ 26.remove-duplicates-from-sorted-array_test.go
@@ -0,0 +1,57 @@
+package main
+
+import "testing"
+
+var removeDuplicatesCases = []struct {
+	name   string
+	input  []int
+	except int
+}{
+	{
+		"test1",
+		[]int{1, 1, 2},
+		2,
+	},
+
+	{
+		"test2",
+		[]int{0, 0, 1, 1, 1, 1, 2, 3, 4, 4},
+		5,
+	},
+
+	{
+		"test3",
+		[]int{0, 0, 0, 0, 0},
+		1,
+	},
+
+	{
+		"test5",
+		[]int{1},
+		1,
+	},
+}
+
+func TestRemoveDuplicates(t *testing.T) {
+	t.Parallel()
+	for _, c := range removeDuplicatesCases {
+		t.Run(c.name, func(t *testing.T) {
+			actual := removeDuplicates(c.input)
+			if actual != c.except {
+				t.Errorf("removeDuplicates %s test  has fail: input:%v ,except:%v, actual:%v \n", c.name, c.input, c.except, actual)
+			}
+		})
+	}
+}
+
+func TestRemoveDuplicates2(t *testing.T) {
+	t.Parallel()
+	for _, c := range removeDuplicatesCases {
+		t.Run(c.name, func(t *testing.T) {
+			actual := removeDuplicates2(c.input)
+			if actual != c.except {
+				t.Errorf("removeDuplicates %s test  has fail: input:%v ,except:%v, actual:%v \n", c.name, c.input, c.except, actual)
+			}
+		})
+	}
+}
