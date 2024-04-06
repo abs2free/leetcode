fa4108e01205c69e1ef733a7ac5e6e22e7c7923b
abs2free <abs2free@gmail.com>
2024-04-06 17:26:47 +0800

    27. 移除元素

---
+++ 0027.remove-element_test.go
@@ -0,0 +1,90 @@
+package main
+
+import "testing"
+
+// para 是参数
+// one 代表第一个参数
+type para27 struct {
+	one []int
+	two int
+}
+
+// ans 是答案
+// one 代表第一个答案
+type ans27 struct {
+	one int
+}
+
+var removeElementCases = []struct {
+	name   string
+	input  para27
+	except ans27
+}{
+	{
+		"test1",
+		para27{[]int{1, 0, 1}, 1},
+		ans27{1},
+	},
+
+	{
+		"test2",
+		para27{[]int{0, 1, 0, 3, 0, 12}, 0},
+		ans27{3},
+	},
+
+	{
+		"test3",
+		para27{[]int{0, 1, 0, 3, 0, 0, 0, 0, 1, 12}, 0},
+		ans27{4},
+	},
+
+	{
+		"test4",
+		para27{[]int{0, 0, 0, 0, 0}, 0},
+		ans27{0},
+	},
+
+	{
+		"test5",
+		para27{[]int{1}, 1},
+		ans27{0},
+	},
+
+	{
+		"test6",
+		para27{[]int{0, 1, 2, 2, 3, 0, 4, 2}, 2},
+		ans27{5},
+	},
+}
+
+func TestRemoveElement(t *testing.T) {
+	for _, c := range removeElementCases {
+		t.Run(c.name, func(t *testing.T) {
+			actual := removeElement(c.input.one, c.input.two)
+			if actual != c.except.one {
+				t.Errorf("removeElement %s test  has fail: input:%v ,except:%v, actual:%v \n", c.name, c.input, c.except, actual)
+			}
+		})
+	}
+}
+
+func TestRemoveElement2(t *testing.T) {
+	for _, c := range removeElementCases {
+		t.Run(c.name, func(t *testing.T) {
+			actual := removeElement2(c.input.one, c.input.two)
+			if actual != c.except.one {
+				t.Errorf("removeElement %s test  has fail: input:%v ,except:%v, actual:%v \n", c.name, c.input, c.except, actual)
+			}
+		})
+	}
+}
+func TestRemoveElement3(t *testing.T) {
+	for _, c := range removeElementCases {
+		t.Run(c.name, func(t *testing.T) {
+			actual := removeElement3(c.input.one, c.input.two)
+			if actual != c.except.one {
+				t.Errorf("removeElement %s test  has fail: input:%v ,except:%v, actual:%v \n", c.name, c.input, c.except, actual)
+			}
+		})
+	}
+}
