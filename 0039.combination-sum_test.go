daabc4f858827a2ad1d83bb525cc4b0a4000b348
abs2free <abs2free@gmail.com>
2024-04-10 16:24:09 +0800

    39. 组合总和

---
+++ 0039.combination-sum_test.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+// para 是参数
+// one 代表第一个参数
+type para39 struct {
+	n []int
+	k int
+}
+
+// ans 是答案
+// one 代表第一个答案
+type ans39 struct {
+	one [][]int
+}
+
+var combinationSumCases = []struct {
+	name   string
+	input  para39
+	except ans39
+}{
+	{
+		"test1",
+		para39{[]int{2, 3, 6, 7}, 7},
+		ans39{[][]int{{7}, {2, 2, 3}}},
+	},
+
+	{
+		"test2",
+		para39{[]int{2, 3, 5}, 8},
+		ans39{[][]int{{3, 5}, {2, 3, 3}, {2, 2, 2, 2}}},
+	},
+}
+
+func TestCombinationSum(t *testing.T) {
+	t.Parallel()
+	for _, c := range combinationSumCases {
+		t.Run(c.name, func(t *testing.T) {
+			actual := combinationSum(c.input.n, c.input.k)
+			if !reflect.DeepEqual(actual, c.except.one) {
+				t.Errorf("combinationSum %s test  has fail: input:%v ,except:%v, actual:%v \n", c.name, c.input, c.except, actual)
+			}
+		})
+	}
+}
